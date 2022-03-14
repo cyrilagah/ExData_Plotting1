317d8658442fb888917238deb5bf5a0181879cd2
cyrilagah <66671576+cyrilagah@users.noreply.github.com>
2022-03-14 13:48:16 +0100

    Add files via upload
    Assignment

---
+++ plot1.R
@@ -0,0 +1,12 @@
+##reading the data, assigning the variables and sub-setting the power consumption data
+
+power <- read.table("C:/Users/GH-EMB-IT-01/Desktop/datasciencecoursera/household_power_consumption.txt", skip = 1, sep = ";")
+names(power) <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+subpower <- subset(power, power$Date == "1/2/2007" | power$Date == "2/2/2007")
+
+
+##plotting the graph
+
+hist(as.numeric(as.character(subpower$Global_active_power)), col='red', main= "Global Active Power", xlab="Global Active Power(kilowatts)")
+
+title(main="Global Active Power")
